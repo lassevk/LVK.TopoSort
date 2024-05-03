575b708c81cbb9ac391492ff47e860d07d72623b
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2024-05-03 10:58:47 +0200

    Add conversion operator and extension method tests

--- src/LVK.TopoSort.Tests/TopoTests.cs
+++ src/LVK.TopoSort.Tests/TopoTests.cs
@@ -5,7 +5,7 @@ public class TopoTests
     [TestCase(1, 2)]
     [TestCase(2, 3)]
     [TestCase(2, 1)]
-    public void Create_PairOfElementsWithTestCases_CreatesCorrectConstraints(int first, int second)
+    public void Constraint_WithTestCases_CreatesCorrectConstraints(int first, int second)
     {
         Constraint<int> constraint = Topo.Constrained(first, second);
 
@@ -16,9 +16,31 @@ public class TopoTests
     [TestCase(1)]
     [TestCase(2)]
     [TestCase(3)]
-    public void Create_SingleElementWithTestCases_CreatesCorrectConstraints(int element)
+    public void Element_WithTestCases_CreatesCorrectConstraints(int element)
     {
         Constraint<int> constraint = Topo.Element(element);
+
+        Assert.That(constraint.FirstElement, Is.EqualTo(element));
+        Assert.That(constraint.SecondElement, Is.EqualTo(element));
+    }
+
+    [TestCase(1, 2)]
+    [TestCase(2, 3)]
+    [TestCase(2, 1)]
+    public void FollowedBy_WithTestCases_CreatesCorrectConstraints(int first, int second)
+    {
+        Constraint<int> constraint = first.FollowedBy(second);
+
+        Assert.That(constraint.FirstElement, Is.EqualTo(first));
+        Assert.That(constraint.SecondElement, Is.EqualTo(second));
+    }
+
+    [TestCase(1)]
+    [TestCase(2)]
+    [TestCase(3)]
+    public void ConversionOperator_WithTestCases_CreatesCorrectConstraints(int element)
+    {
+        Constraint<int> constraint = element;
 
         Assert.That(constraint.FirstElement, Is.EqualTo(element));
         Assert.That(constraint.SecondElement, Is.EqualTo(element));
